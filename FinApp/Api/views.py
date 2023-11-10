61885c98826ed003abad20f512aa5f4321a27b38
Thea Banis <101121142+inevitableBARIS@users.noreply.github.com>
2023-11-10 16:19:18 -0700

    Update views.py
    added supplementary comments to improve the code's readability and highlight function processes.

--- FinApp/Api/views.py
+++ FinApp/Api/views.py
@@ -1,3 +1,4 @@
+#IMPORT MODULES AND CLASSES
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
@@ -17,7 +18,11 @@ import time
 import datetime
 from django.contrib.auth.decorators import login_required
 import os
+
+#LOAD VARIABLES FROM ENVIRONMENT FILE
 load_dotenv()
+
+#CONFIGURE PLAID API SETTINGS
 configuration = plaid.Configuration(
     host=plaid.Environment.Sandbox,
     api_key={
@@ -25,41 +30,50 @@ configuration = plaid.Configuration(
         'secret': os.getenv('SecretToken'),
     }
 )
+
+#CREATE PLAID API CLIENT
 api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
 
+#FUNCTION DEFINITION - RETRIEVE ACCOUNT INFORMATION
 def getAccountinfo(request):
     """
     :Returns a plaid list of accounts with the cardId account num
     :param django request: request from a valid view
     """
+    #RETRIEVE INPUT FROM SESSION
     CardList = request.session.get('CardData', None)
     RequestedcardID= request.GET.get('CardId')
     accountNum=""
     institution_token=""
     try:    
+        #INPUT VALIDATION CHECK - CHECK IF CARDLIST EXISTS
         if CardList==None:
             raise
+        #PROCESS - ITERATE THROUGH CARDLIST TO FIND MATCHING REQUESTED CARD    
         for item in CardList:
             if int(RequestedcardID)== item["cardId"]:
                 accountNum=item["accountNum"]
                 test=institution.objects.filter(institutionID=item["instituion"])
                 institution_token=test[0].access_token
-    
+                
+        #PROCESS - CREATE PLAID API REQUEST FOR ACCOUNT INFORMATION
         plaidRequest = plaid_api.AccountsGetRequest(
             access_token=institution_token,
             options=AccountsGetRequestOptions(account_ids=[accountNum])
         )
-    
+
+        #PROCESS - REQUEST
         PlaidResponse = client.accounts_get(plaidRequest)
+        #OUTPUT - RESULT
         return PlaidResponse['accounts']
     except Exception as e:
-        # Handle the exception
+        #EXCEPTION HANDLING: RAISE FLAG
         raise e
 
 
 
-#Send the client a link to setup Plaid institution
+#FUNCTION DEFINITION - SEND CLIENT LINK REQUEST TO PLAID FOR SETUP
 @login_required
 def sendLinkRequest(request):  
     """
@@ -91,7 +105,8 @@ def sendLinkRequest(request):
         return JsonResponse(response.to_dict())
     except:
          return JsonResponse({'Error': 'Failed'})
-#receive and setup a new pulic acess token and save to database
+        
+#FUNCTION DEFINITION - receive and setup a new pulic acess token and save to database
 @login_required
 def setupInstitution(request):
     """
@@ -107,23 +122,32 @@ def setupInstitution(request):
     :rtype: JOSN.Response that verifies the successful creation of an new instituation.
     :raises: requests.exceptions.RequestException if the request fails.
     """
+    #PROCESS - SETUP NEW INSTITUTION WITH PUBLIC TOKEN
     try:
+        #INPUT VALIDATION - CHECK IF REQUEST METHOD IS POST
         if request.method != 'POST':
             return JsonResponse({'error': 'Invalid request method'}, status=405)
+        #PROCESS - RETRIEVE PUBLIC TOKEN FROM THE POST REQUEST    
         link_public_token = request.POST.get('public_token')
+        #AUTHENTICATION - EXCHANGE PUBLIC TOKEN FOR PRIVATE
         if link_public_token:
             exchange_request = plaid_api.ItemPublicTokenExchangeRequest(
                 public_token= link_public_token
                 )
             exchange_response = client.item_public_token_exchange(exchange_request)
             private_token=exchange_response['access_token']
+
+            #PROCESS - SAVE THE NEW INSTITUTION TO DATABASE
             new_institution = institution(user=request.user, access_token=private_token)
             new_institution.save()
+
+            #OUTPUT - RETURN SUCCESS JSON RESPONSE
             return JsonResponse({'Setup': 'complete'})
 
     except:
          return JsonResponse({'Error': 'Failed'})
 
+#FUNCTION DEFINITION - RETRIEVE TRANSACTIONS BASED ON ACCOUNT/DATE RANGE.
 @login_required
 def getTransactions(request):
     """
@@ -139,8 +163,10 @@ def getTransactions(request):
     :rtype: JOSN.Response
     :raises: requests.exceptions.RequestException if the request fails.
     """
+    #INPUT VALIDATION CHECK - CHECK IF THE REQUEST METHOD IS GET
     if request.method != 'GET':
         return HttpResponseBadRequest("Invalid request method")
+    #PROCESS - RETRIEVE INPUT PARAMETERS FROM GET REQUEST    
     RequestedcardID= request.GET.get('CardId')
     start= request.GET.get('StartDate')
     end= request.GET.get('StopDate')
@@ -148,21 +174,27 @@ def getTransactions(request):
     accountNum=""
     institution_token=""
     try:    
+        #PROCESS - RETRIEVE CARD INFORMATION FROM SESSION
         CardList = request.session.get('CardData', None)
+        #INPUT VALIDATION CHECK - CHECK IF CARD EXISTS
         if CardList==None:
             raise
+        #PROCESS - ITERATE THROUGH REQUESTED CARD 
         for item in CardList:
             if int(RequestedcardID)== item["cardId"]:
                 accountNum=item["accountNum"]
                 test=institution.objects.filter(institutionID=item["instituion"])
                 institution_token=test[0].access_token
+        #PROCESS - CREATE PLAID API REQUEST FOR ACCOUNT INFORMATION
         plaidRequest = plaid_api.TransactionsGetRequest(
             access_token=institution_token,
             options=TransactionsGetRequestOptions(account_ids=[accountNum]),
             start_date= datetime.datetime.strptime(start, '%Y-%m-%d').date(),
             end_date= datetime.datetime.strptime(end, '%Y-%m-%d').date()
         )
+        #PROCESS - REQUEST
         response = client.transactions_get(plaidRequest)
+        #DATA TRANSFORMATION - FORMAT TO BE USER READIBLE 
         transactions = response['transactions']
         transactionList=[]
         print(len(transactions))
@@ -182,11 +214,14 @@ def getTransactions(request):
                 "amount":amount,
                 "date": transaction.date.strftime("%Y-%m-%d")    
             })
+        #OUTPUT - RETURN THE FORMATTED TRANSACTION DATA AS A JSON RESPONSE    
         return JsonResponse(transactionList,safe=False)
+    #EXCEPTION HANDLING - RAISE FLAG AND OUTPUT DESCRIPTION    
     except Exception as e:
         # Handle the exception
         return JsonResponse({'Error': 'Failed'})   
 
+#FUNCTION DEFINITION - GET ACCOUNT BALANCE
 @login_required
 def getBalance(request):
     """
@@ -203,20 +238,22 @@ def getBalance(request):
     :raises: requests.exceptions.RequestException if the request fails.
     """
     try:
-
+        #INPUT VALIDATION CHECK - CHECK IF THE REQUEST METHOD IS GET
         if(request.method!='GET'):
             return HttpResponseBadRequest("Invalid request method")
+        #PROCESS - RETRIEVE ACCOUNT INFORMATION USING GETACCOUNTINFO FUNC()
         accounts=getAccountinfo(request)
         AvailableBalance=accounts[int(0)]['balances']["available"]
         AccountName=accounts[int(0)]['official_name']
+        #DATA TRANSFORMATION - FORMAT TO BE USER READIBLE 
         if AvailableBalance== None:
             AvailableBalance="Not available"
         else:
             AvailableBalance="$"+str(AvailableBalance)
         return JsonResponse({"AccountName":AccountName,
                             "Balance":AvailableBalance })
+    #EXCEPTION HANDLING - RAISE FLAG AND OUTPUT DESCRIPTION   
     except Exception as e:
-        # Handle the exception
         return JsonResponse({'Error': 'Failed'})
 
 @login_required
@@ -236,16 +273,22 @@ def getCardName(request):
     """
     
     try:
+        #INPUT VALIDATION CHECK - CHECK IF THE REQUEST METHOD IS GET
         if(request.method!='GET'):
             return HttpResponseBadRequest("Invalid request method")
+        #PROCESS - RETRIEVE INPUT PARAMETERS FROM GET REQUEST  
         RequestedcardID= request.GET.get('CardId')
+        #PROCESS - RETRIEVE ACCOUNT INFORMATION USING GETACCOUNTINFO FUNC()
         accounts=getAccountinfo(request)
         accountName=accounts[0]["name"]
+        #PROCESS - APPEND ADDITIONAL INFO BASED ON CARDID
         if int(RequestedcardID) >=1000 and int(RequestedcardID)<2000:
             accountName+=" Transactions"
         elif int(RequestedcardID) >=2000 and int(RequestedcardID)<3000:
             accountName+=" Balance"
+        #DATA TRANSFORMATION - FORMAT TO BE USER READIBLE 
         return JsonResponse({"CardName":accountName})
+    #EXCEPTION HANDLING - RAISE FLAG AND OUTPUT DESCRIPTION   
     except Exception as e:
         # Handle the exception
         return JsonResponse({'Error': 'Failed'})
